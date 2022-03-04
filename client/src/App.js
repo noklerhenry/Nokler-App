57b6ec2512df658c0332e84f4bdeb38f7b860d74
Bruno Marinich <brunomarinich@gmail.com>
2022-03-04 16:32:43 -0300

    Cart and Nav done

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 // import { Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import { Home } from './components/Home';
-=======
-import logo from './logo.svg';
-import './App.css';
-import { Home } from './Components/Home';
->>>>>>> 961014b368f689373d5a2b9a3eab569d55e521cf
 
 function App() {
   return (
     <div>
       <header className="App-header">
-<<<<<<< HEAD
         <NavBar />
-=======
-        <img src={logo} className="App-logo" alt="logo" />
->>>>>>> 961014b368f689373d5a2b9a3eab569d55e521cf
         <Home />
       </header>
     </div>
