56a49f18ef07c5973fe1029a97c52a36c24d07ea
Bruno Marinich <brunomarinich@gmail.com>
2022-03-04 17:24:55 -0300

    cart and nav done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Router, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import { Home } from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
+        <Home />
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './Store';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Auth0Provider
+      domain='dev-4dwzec6x.us.auth0.com'
+      clientId='oDVbYUSJ60qpeBqm9h0OHLNsrTygI24q'
+      redirectUri={window.location.origin}>
+        <App />
+      </Auth0Provider>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
