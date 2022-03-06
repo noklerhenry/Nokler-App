85760026a027198dee330bb12bb7a230cdd65d75
Patricio Zubiri <pzubiri@gmail.com>
2022-03-06 17:37:50 -0300

    subidaDemo

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './Store';
 import { Auth0Provider } from '@auth0/auth0-react';
-import  theme  from './Styles/Theme';
+import  theme  from './Styles/theme';
 import '@fontsource/anton/400.css'
 
 ReactDOM.render(
